413b2acafdb4e4a9fbbe389fab79679629e2afd4
codecivil <codecivil@schwentine.local>
2020-05-01 13:49:35 +0200

    changelog updated

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "28.04.2020";
-$versionnumber = "1.1.6";
+$versiondate = "1.5.2020";
+$versionnumber = "1.1.7";
 ?>
