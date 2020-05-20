6ffca44f0787bc52bc0a820da1fc3ba16b64b7ad
codecivil <kuehnel@codecivil.de>
2020-05-20 18:26:54 +0200

    info.php updated

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "19.5.2020";
-$versionnumber = "1.2.6";
+$versiondate = "20.05.2020";
+$versionnumber = "1.2.7";
 ?>
