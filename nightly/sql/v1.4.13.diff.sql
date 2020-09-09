4888dc8c56c91c735d3e2d019f855a886d2ab745
codecivil <kuehnel@codecivil.de>
2020-09-09 14:31:19 +0200

    log button in openStatAdmin

--- nightly/sql/v1.4.13.diff.sql
+++ nightly/sql/v1.4.13.diff.sql
@@ -1,9 +1,3 @@
 -- add field subtablemachine to _permissions-tables and grant permissions to parentmachine in os_tables
 -- this uses the 'openStat-MySQL-extension' 'OS_TABLES LIKE' (see preprocesing in function importSQL); formulate query as if for one table and replace table name by 'OS_TABLES LIKE <PATTERN>'
 ALTER TABLE OS_TABLES LIKE '%_permissions' ADD COLUMN subtablemachine VARCHAR(40);
-
--- next step: grant permissions on column subtablemachine to all users
--- problem: @allow is overwritten before executed!
---SELECT @allow:=CONCAT('GRANT SELECT (subtablemachine) ON OS_TABLES LIKE \'%_permissions\' TO ', (SELECT GROUP_CONCAT(rolename) from os_roles where rolename != '_none_') )GRANT SELECT (subtablemachine) ON OS_TABLES LIKE '%_permussions' TO GROUP_CONCAT(rolename from os_roles);
---PREPARE stmt from @allow;
---EXECUTE stmt;
