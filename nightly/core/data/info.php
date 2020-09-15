39fd017fd666311f3ecfad4f74f1e974b5603791
codecivil <kuehnel@codecivil.de>
2020-09-15 15:28:39 +0200

    bugfix: import of single dates failed

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "09.09.2020";
-$versionnumber = "1.4.14";
+$versiondate = "15.09.2020";
+$versionnumber = "1.4.16";
 ?>
