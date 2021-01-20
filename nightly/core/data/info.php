ddd4023dab9df8ba5a7c64a8d1dd60e9bddaee30
codecivil <codecivil@caspiansea.local>
2021-01-20 18:56:53 +0100

    bugfix: adding multiple classes got incompatible

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "18.01.2021";
-$versionnumber = "1.5.0";
+$versiondate = "20.01.2021";
+$versionnumber = "1.5.2";
 ?>
