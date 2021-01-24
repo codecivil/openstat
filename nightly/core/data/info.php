fe6f44df32c26641e132b0bb8cb802aeddfe4f19
codecivil <codecivil@caspiansea.local>
2021-01-24 18:39:13 +0100

    bugfix: numbers were not summed up in statTable

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "23.01.2021";
-$versionnumber = "1.5.3";
+$versiondate = "24.01.2021";
+$versionnumber = "1.5.5";
 ?>
