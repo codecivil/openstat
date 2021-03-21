f34f8dbc5d678f2881227bea592b0dbff5f89dbb
codecivil <codecivil@caspiansea.local>
2021-03-21 19:51:26 +0100

    bugfix: exportRefuKey did not handle umlaute well

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "08.02.2021";
-$versionnumber = "1.5.13";
+$versiondate = "21.03.2021";
+$versionnumber = "1.5.14";
 ?>
