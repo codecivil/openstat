1c73b7e4b4654a50fa4b2615aa8533cff7d13b29
codecivil <codecivil@caspiansea.local>
2021-03-21 19:43:31 +0100

    bugfix: exportRefuKey did not handle umlaute well

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "06.02.2021";
-$versionnumber = "1.5.11";
+$versiondate = "08.02.2021";
+$versionnumber = "1.5.13";
 ?>
---
+++ nightly/plugins-config/tinymce/js/os_tinymce.js
@@ -0,0 +1,13 @@
+function tinyMCEinit() {
+	tinyMCE.init({
+	   selector: 'textarea.editor',  //Change this value according to your HTML
+	   width: "40rem",
+	   height: "40rem",
+	   plugins: "lists",
+	   toolbar: "undo redo | styleselect | bold italic | numlist | bullist | link image | forecolor | backcolor",
+	   browser_spellcheck: true,
+	   entity_encoding: "raw", //default is "named", but Umlaute in HTML entities is not supported by (ODF1.2) XML, so throws error in refuKey-Export
+  	   gecko_spellcheck: true,
+	});
+}
+
