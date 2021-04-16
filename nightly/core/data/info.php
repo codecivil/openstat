efd0c5b5f3bdf02d6be26ecb8b883fd6ec3277db
codecivil <codecivil@caspiansea.local>
2021-04-16 17:23:38 +0200

    changelogs updated

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,6 +1,6 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "08.04.2021";
-$versionnumber = "1.5.17";
+$versiondate = "16.04.2021";
+$versionnumber = "1.5.18";
 ?>
