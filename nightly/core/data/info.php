07e9be4eb5e6678a3823a2416b4e63ddd2500cf4
codecivil <codecivil@caspiansea.local>
2021-12-24 14:21:10 +0100

    some more fixes for the resdesign in v1.6.0

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "23.12.2021";
-$versionnumber = "1.6.2";
+$versiondate = "24.12.2021";
+$versionnumber = "1.6.3";
 $contact = "openstat@codecivil.de";
 ?>
