f2e8fba7862605f088ab39be345f24422a4d31eb
codecivil <codecivil@caspiansea.local>
2022-06-20 17:47:12 +0200

    sql preparation for major update

---
+++ nightly/sql/v1.7.3.diff.sql
@@ -0,0 +1,5 @@
+-- allow access to subtablemachine field for everyone
+GRANT SELECT (subtablemachine) ON OS_TABLES LIKE '%_permissions' TO OS_ROLES LIKE '%';
+
+-- implement identifiers
+ALTER TABLE os_tables ADD COLUMN identifiers TEXT DEFAULT NULL;
