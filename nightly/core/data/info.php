abcbd14f49b13f8710599f081e9bdd4b791eb369
codecivil <codecivil@caspiansea.local>
2022-09-29 09:43:18 +0200

    changelog adapted

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "25.08.2022";
-$versionnumber = "1.7.6";
+$versiondate = "29.09.2022";
+$versionnumber = "1.7.7";
 $contact = "openstat@codecivil.de";
 ?>
