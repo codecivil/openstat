78cd1b48b2e2bdd90d9bd731323ca59200497285
codecivil <codecivil@caspiansea.local>
2023-06-29 16:05:52 +0200

    major uxp improvements

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "06.05.2023";
-$versionnumber = "1.7.9";
+$versiondate = "30.06.2023";
+$versionnumber = "1.7.10";
 $contact = "openstat@codecivil.de";
 ?>
