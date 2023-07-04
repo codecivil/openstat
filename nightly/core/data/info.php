c50e9518a6587d83568a3d55abc34d3bb5c35524
codecivil <codecivil@caspiansea.local>
2023-07-04 14:28:41 +0200

    sidebar improvements

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "03.07.2023";
-$versionnumber = "1.7.11";
+$versiondate = "04.07.2023";
+$versionnumber = "1.7.12";
 $contact = "openstat@codecivil.de";
 ?>
