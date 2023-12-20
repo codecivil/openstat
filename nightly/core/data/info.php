c0daa021b3c2b8eb68781d0b5c0a51b347be9eb9
codecivil <codecivil@caspiansea.local>
2023-12-20 18:05:10 +0100

    flags and modifiers for field functions

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -2,6 +2,6 @@
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
 $versiondate = "20.12.2023";
-$versionnumber = "1.7.18";
+$versionnumber = "1.7.19";
 $contact = "openstat@codecivil.de";
 ?>
