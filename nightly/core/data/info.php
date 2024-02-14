cc862181deaba368bc5f2a0c99119c833231f744
codecivil <codecivil@caspiansea.local>
2024-02-14 10:58:49 +0100

    new entries support show/hide

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "05.02.2024";
-$versionnumber = "1.7.21";
+$versiondate = "14.02.2024";
+$versionnumber = "1.7.22";
 $contact = "openstat@codecivil.de";
 ?>
