f957a5d52d1de4a9cbfaead29aa83af8b0def47d
codecivil <codecivil@caspiansea.local>
2024-08-27 11:39:27 +0200

    cleanup of error.log

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,7 +1,7 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "??.??.2024";
+$versiondate = "27.08.2024";
 $versionnumber = "1.8.8";
 $contact = "openstat@codecivil.de";
 ?>
