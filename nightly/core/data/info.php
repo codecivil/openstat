c0aca9c6aceeeec082397e2d5257e369668b5084
codecivil <codecivil@caspiansea.local>
2024-11-28 13:36:05 +0100

    fixes for subtable layout

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,8 +1,8 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "22.11.2024";
-$versionnumber = "1.8.9";
+$versiondate = "28.11.2024";
+$versionnumber = "1.8.10";
 $firefox_least_featureversion = "121";
 $contact = "openstat@codecivil.de";
 ?>
