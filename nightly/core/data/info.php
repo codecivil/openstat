c8c22c2a757c4f0b03ce0bbbd1553bcc69a13934
codecivil <codecivil@caspiansea.local>
2024-12-11 17:16:34 +0100

    manual for subtables

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,8 +1,8 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "28.11.2024";
-$versionnumber = "1.8.10";
+$versiondate = "11.12.2024";
+$versionnumber = "1.8.11";
 $firefox_least_featureversion = "121";
 $contact = "openstat@codecivil.de";
 ?>
