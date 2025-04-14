8dda60a1d56e3d7ba37fa8c98f96a8a17e126c21
codecivil <codecivil@caspiansea.local>
2025-04-14 15:17:44 +0200

    ENV variables in field functions

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -2,7 +2,7 @@
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
 $versiondate = "14.04.2025";
-$versionnumber = "1.12.1";
+$versionnumber = "1.13.0";
 $firefox_least_featureversion = "121";
 $chrome_least_version = "105";
 $contact = "openstat@codecivil.de";
