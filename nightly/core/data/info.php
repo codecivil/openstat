ecd0cb1f27fb911d7cb61ee2fe6b4420416f8b13
codecivil <codecivil@caspiansea.local>
2025-05-02 15:27:32 +0200

    several possible paths for templates

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,8 +1,8 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "30.04.2025";
-$versionnumber = "1.14.2";
+$versiondate = "02.05.2025";
+$versionnumber = "1.14.3";
 $firefox_least_featureversion = "121";
 $chrome_least_version = "105";
 $contact = "openstat@codecivil.de";
