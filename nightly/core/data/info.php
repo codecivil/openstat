16ae7c3232e9b80761b9fce3ff4db41646a5f1aa
codecivil <codecivil@caspiansea.local>
2025-07-23 17:49:20 +0200

    minor improvements

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,8 +1,8 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "15.07.2025";
-$versionnumber = "1.16.0";
+$versiondate = "23.07.2025";
+$versionnumber = "1.16.1";
 $firefox_least_featureversion = "121";
 $chrome_least_version = "105";
 $contact = "openstat@codecivil.de";
