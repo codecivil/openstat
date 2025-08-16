9207261425eebc0e0bff71d75a50518fcee0f1c7
codecivil <codecivil@caspiansea.local>
2025-08-16 11:58:15 +0200

    video manual for info templates

--- nightly/core/data/info.php
+++ nightly/core/data/info.php
@@ -1,8 +1,8 @@
 <?php
 $author = "codecivil Dr. Marco Kühnel ICT Services";
 $license = "GPLv3";
-$versiondate = "06.08.2025";
-$versionnumber = "1.19.0";
+$versiondate = "16.08.2025";
+$versionnumber = "1.19.2";
 $firefox_least_featureversion = "121";
 $chrome_least_version = "105";
 $contact = "openstat@codecivil.de";
